7489ab64180553bbab76faaee916a4f615aa4a6c
User <User@192.168.1.8>
2022-07-02 23:45:27 +0300

    HW#6 Strings

---
+++ src/telran/test/Strings.java
@@ -0,0 +1,70 @@
+package telran.test;
+
+	public class Strings {
+		/**
+		 * 
+		 * @param str1 ascii string with no repeated symbols
+		 * @param str2 ascii string with no repeated symbols
+		 * @return array with two numbers
+		 * first - number of the symbols of the str1 that exist in str 2 at the same indexes 
+		 * second - number of the symbols of the str1 that exist in str 2 at differ indexes
+		 */
+	public static int[] deepNoRepeatedCompare (String str1,String str2){
+		//Two implementations
+		//first: additional helper array such array['a'] 
+		// is an index of the symbol 'a' or -1
+		//
+		// example, str1-"ctab" then array['a'] =2;
+		//******************************************
+		//second: implementation based on the method indexOf
+		char ar1[]=str1.toCharArray();
+		char ar2[]=str2.toCharArray();
+		int res[]=  {0,0};
+		int res1=0;
+		int res2=0;
+		for (int i=0; i<str1.length();i++) {
+			if ((i<str2.length())&&(ar1[i]==ar2[i])) {
+			res1++;}
+			else
+			{			
+		    if (str2.indexOf(ar1[i])>-1)
+		    {res2++;}
+			}
+			
+		}
+		res[0]=res1;
+		res[1]=res2;
+		
+		return res;
+	}
+	/**
+	 * 
+	 * @param str1 English letters(may have repeats)
+	 * @param str2  English letters(may have repeats)
+	 * @return true if:
+	 * (1)str2 has the same as str1 length
+	 *(2)str2 comprises of all letters from str1
+	 **/
+	public static boolean isAnagram(String str1, String str2) {
+	// additional helper array such that array['a'] is the number of 'a' occurences in str1
+	// str1="hello", array['l']=2; array['w'=0;]
+		boolean res=false;
+		if (str1.length()==str2.length()) {
+		char ar1[]=str1.toCharArray();
+		
+		String str_res=new String(str2);
+		
+		int i=0;
+		while (i<str1.length()&&str_res.indexOf(ar1[i])!=-1) {
+			String str=str_res.substring(0, 0)+str_res.substring(1);
+			str_res=str;
+			i++;
+		}
+		if (str_res.length()==0) {res=true;}
+	
+		}
+		return res;
+		
+	}
+	}
+
---
+++ src/telren/text/tests/StringsClassTests.java
@@ -0,0 +1,39 @@
+package telren.text.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import telran.test.Strings;
+
+class StringsClassTests {
+
+	@Test
+	void deepCompareTest() {
+	String str1="123,tT89";
+	String str2="123,tT89";
+	String str3="19";
+	String str4="kugy";
+	String str5="98Tt,321";
+	int expected1[]= {8,0};
+	int  expected2[]= {1,1};
+	int expected4[]= {0,8};
+	int expected3[]= {0,0};
+    assertArrayEquals(expected1,Strings.deepNoRepeatedCompare(str1,str2));
+	assertArrayEquals(expected2,Strings.deepNoRepeatedCompare(str1,str3));
+	assertArrayEquals(expected3,Strings.deepNoRepeatedCompare(str1,str4));
+	assertArrayEquals(expected4,Strings.deepNoRepeatedCompare(str1,str5));
+
+	}
+@Test 
+void isAnagramTests() {
+	String str1= "Hello";
+	String str2="Helll";
+	String str3="elloH";
+	String str4="Helo";
+//	assertTrue(Strings.isAnagram(str1, str3));
+//	assertFalse(Strings.isAnagram(str1, str2));
+	assertFalse(Strings.isAnagram(str1, str4));
+	assertTrue(Strings.isAnagram(str1, str1));
+}
+}
